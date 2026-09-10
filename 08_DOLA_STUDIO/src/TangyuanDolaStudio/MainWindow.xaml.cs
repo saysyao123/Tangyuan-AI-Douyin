67c31075753f6a536456e6e9fb4e757b6cf54aba
saysyao123 <saysyao@googlemail.com>
2026-09-10 11:35:08 +0800

    fix(dola-studio): import System.IO in main window

--- 08_DOLA_STUDIO/src/TangyuanDolaStudio/MainWindow.xaml.cs
+++ 08_DOLA_STUDIO/src/TangyuanDolaStudio/MainWindow.xaml.cs
@@ -1,6 +1,7 @@
 using System.Collections.ObjectModel;
 using System.ComponentModel;
 using System.Diagnostics;
+using System.IO;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Input;
